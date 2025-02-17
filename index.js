7516da9f6092585410f55e60381c582c88e6ff35
JoAnisky <anisky5@gmail.com>
2025-02-17 12:20:40 +0100

    fix listening port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
     res.send('Hello from the containerized app!');
 });
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+    console.log(`App listening at https://app.jonathanlore.fr:${port}`);
 });
