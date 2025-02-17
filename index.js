8d689538fe32fbe5736d4db9fc4a8dd710e75319
Jonathan Loré <99493324+JoAnisky@users.noreply.github.com>
2025-02-17 13:55:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello from the containerized app!');
+    res.send('Test du Webhook Git!');
 });
 
 app.listen(port, () => {
